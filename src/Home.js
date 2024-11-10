a1fbe5adbd752c6fc91b7c620e6a9648a8916b23
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-11-10 09:40:12 +0100

    props

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,17 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
 
-    const handleClick = () => {
-        console.log("hello, ninjas")
-    };
-
-    const handleClickAgain = (name) => {
-        console.log('hello' + name)
-    }
-
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={ () => { handleClickAgain("Petrus")}}>Click me again</button>
+            <BlogList blogs={blogs} title="All Blogs!"/>
         </div>
      );
 }
