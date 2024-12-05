55e80b3d5633bc667fd37b5718277172109eee97
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-12-05 16:09:55 +0100

    Router

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./use/useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter( (blog) => blog.id !== id )
-        setBlogs(newBlogs)
-    }
+    const { data, isPending, Error} = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {Error && <div>{Error}</div>}
+            {isPending && <div> Pending ... </div>}
+            {data && <BlogList blogs={data} title="All Blogs!"/>}
         </div>
      );
 }
